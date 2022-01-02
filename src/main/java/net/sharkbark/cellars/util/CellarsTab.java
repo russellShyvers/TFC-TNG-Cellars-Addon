75e553033e80b98923243a8fd955500639e65b9c
SharkeyMyrl <russell.shyvers@sjsu.edu>
2022-01-01 20:20:00 -0800

    Fixed a issue that popped up with newer versions of forge, will now function with forge 2860.

--- src/main/java/net/sharkbark/cellars/util/CellarsTab.java
+++ src/main/java/net/sharkbark/cellars/util/CellarsTab.java
@@ -7,20 +7,13 @@ import net.sharkbark.cellars.init.ModBlocks;
 
 public class CellarsTab extends CreativeTabs{
 
-
     public CellarsTab() {
         super(Reference.MOD_ID);
-        setBackgroundImageName("item_search.png");
     }
 
     @Override
     public ItemStack createIcon() {
         return new ItemStack(ModBlocks.CELLAR_DOOR);
-    }
-
-    @Override
-    public boolean hasSearchBar() {
-        return true;
     }
 
 }
